89e478da61e532a33e9da2c3752a52057c73d48f
EFFM00 <fischietto.elena@gmail.com>
2023-10-07 10:10:09 -0300

    get user by id router created

--- api/users/user.controller.js
+++ api/users/user.controller.js
@@ -1,5 +1,5 @@
 const { genSaltSync, hashSync } = require("bcryptjs");
-const {create} = require("./user.service");
+const {create, getUserById} = require("./user.service");
 
 module.exports = {
     createUser: (req, res) => {
@@ -16,6 +16,25 @@ module.exports = {
             }
 
             return res.status(200).json({
+                success: 1,
+                data: result
+            })
+        })
+    },
+    getUserById: (req, res) => {
+        const id = req.params.id;
+        getUserById(id, (error, result) => {
+            if(error) {
+                console.error(error);
+                return;
+            }
+            if(!result) {
+                return res.json({
+                    success: 0,
+                    message: "User not found"
+                })
+            }
+            return res.json({
                 success: 1,
                 data: result
             })
--- api/users/user.router.js
+++ api/users/user.router.js
@@ -1,6 +1,8 @@
-const {createUser} = require("./user.controller");
+const {createUser, getUserById} = require("./user.controller");
 const router = require("express").Router();
 
 router.post("/", createUser)
+router.get("/:id", getUserById)
+
 
 module.exports = router;
