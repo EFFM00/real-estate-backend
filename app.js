d2c9188d246cffb596f8d71b7b1dd646662cc146
EFFM00 <fischietto.elena@gmail.com>
2023-10-07 08:51:41 -0300

    router user

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const userRouter = require("./api/users/user.router");
 
 const app = express()
 
@@ -8,6 +9,9 @@ dotenv.config({path: ".env"})
 app.get("/", (req, res) => {
     res.send("HOLA")
 })
+
+app.use("/api/users", userRouter)
+
 
 app.listen(process.env.APP_PORT, () => {
     console.log("Server listening on port " + process.env.APP_PORT);
