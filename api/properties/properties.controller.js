3863743d1ec96ded4418b396d893ae786b7d1eaa
EFFM00 <fischietto.elena@gmail.com>
2023-10-07 11:31:13 -0300

    get property by id done

--- api/properties/properties.controller.js
+++ api/properties/properties.controller.js
@@ -1,4 +1,4 @@
-const { getAllProperties } = require("./properties.service");
+const { getAllProperties, getPropertyById } = require("./properties.service");
 
 module.exports = {
     getAllProperties: (req, res) => {
@@ -12,5 +12,25 @@ module.exports = {
                 data: results
             });
         })
-    }
+    },
+
+    getPropertyById: (req, res) => {
+        const id = req.params.id;
+        getPropertyById(id, (error, result) => {
+        if (error) {
+            console.error(error);
+            return;
+        }
+        if (!result) {
+            return res.json({
+            success: 0,
+            message: "Property not found",
+            });
+        }
+        return res.json({
+            success: 1,
+            data: result,
+        });
+        });
+    },
 }
